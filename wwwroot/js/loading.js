7c9ebb5b588fc9e3f13260dfc305968c650bad37
Hodladi <hodladi@hodla.nu>
2024-07-19 09:21:18 +0200

    Add custom reconnect view

---
+++ wwwroot/js/loading.js
@@ -0,0 +1,28 @@
+﻿function showCustomLoading() {
+    var loadingDiv = document.createElement('div');
+    loadingDiv.className = 'custom-loading';
+    loadingDiv.id = 'customLoadingDiv';
+    loadingDiv.innerHTML = 'Reconnecting...';
+    document.body.appendChild(loadingDiv);
+}
+
+function hideCustomLoading() {
+    var loadingDiv = document.getElementById('customLoadingDiv');
+    if (loadingDiv) {
+        document.body.removeChild(loadingDiv);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    Blazor.start().then(() => {
+        Blazor.defaultReconnectionHandler.onReconnecting = () => {
+            showCustomLoading();
+        };
+        Blazor.defaultReconnectionHandler.onReconnected = () => {
+            hideCustomLoading();
+        };
+        Blazor.defaultReconnectionHandler.onDisconnected = () => {
+            hideCustomLoading();
+        };
+    });
+});
