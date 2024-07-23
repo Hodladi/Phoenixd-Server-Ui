ef7a9855c2df98a6f0ff7216b491c4990db01cc2
Hodladi <hodladi@hodla.nu>
2024-07-23 16:52:40 +0200

    Fix issue with duplicate usage of port 2291 when running application through docker.
    #2

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@ using Wallet.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.Listen(System.Net.IPAddress.Any, 2291);
-});
+// UNCOMMENT THE SECTION BELOW IF YOU DONT RUN THE APPLICATION THROUGH DOCKER
+// IF YOU RUN THE APPLICATION THROUGH DOCKER THEN THE PART BELOW MAKES AN ERROR WITH DUPLICATED USAGE OF PORT 2291
+
+//builder.WebHost.ConfigureKestrel(serverOptions =>
+//{
+//    serverOptions.Listen(System.Net.IPAddress.Any, 2291);
+//});
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
